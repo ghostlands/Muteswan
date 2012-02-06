3a887318409e85cde955a83e9a21153b267b521e
rob <rob@muteswan.org>
2012-02-05 22:02:46 -0500

    Initial draft for WordPress site theme.

---
+++ wp-theme/home.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * the home page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" class="home" role="main">
+
+				<img src="/wp/wp-content/themes/muteswan/i/phone.png" />	
+				<h2>Anonymous friends<br />trusted face-to-face.</h2>
+				<p>Create secure social networks using shared QR codes. Share them with whomever you trust, face-to-face.</p>
+
+				<a href="#market_url" title="Download App">Download App<br />
+					<img src="/wp/wp-content/themes/muteswan/i/market.png" alt="Available in Android Market" />
+				</a>
+
+			</div><!-- #content -->
+			<div style="clear: both;">&nbsp;</div>
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
