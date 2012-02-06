908fe499acd98c8444b08af0d069f4b0d18da63c
James Unger <junger@muteswan.org>
2012-02-05 22:28:22 -0500

    Change root url path to /public to match site.

--- wp-theme/home.php
+++ wp-theme/home.php
@@ -11,12 +11,12 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" class="home" role="main">
 
-				<img src="/wp/wp-content/themes/muteswan/i/phone.png" />	
+				<img src="/public/wp-content/themes/muteswan/i/phone.png" />	
 				<h2>Anonymous friends<br />trusted face-to-face.</h2>
 				<p>Create secure social networks using shared QR codes. Share them with whomever you trust, face-to-face.</p>
 
 				<a href="#market_url" title="Download App">Download App<br />
-					<img src="/wp/wp-content/themes/muteswan/i/market.png" alt="Available in Android Market" />
+					<img src="/public/wp-content/themes/muteswan/i/market.png" alt="Available in Android Market" />
 				</a>
 
 			</div><!-- #content -->
