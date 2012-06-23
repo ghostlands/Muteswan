eeca23531b68fa6204acd3e203f4496c8c256d23
James Unger <junger@muteswan.org>
2012-06-23 14:48:59 -0400

    Drop privileges after request. Set bind ip and port and db name on cmdline.

--- server.go
+++ server.go
@@ -12,10 +12,12 @@ import (
 	"strings"
 	"time"
 	"runtime"
+	"syscall"
+	"flag"
 )
 
 type Msg struct {
-	Iv      string `json:"iv"`
+	Iv      string `json:"iv,omitempty"`
 	Message string `json:"message"`
 }
 
@@ -31,9 +33,9 @@ type Counter struct {
 }
 
 // utility functions
-func updateCounter(id string, s *mgo.Session) int {
+func updateCounter(id string, d *mgo.Database) int {
 	var counter Counter
-	col := s.DB("muteswan").C("counters")
+	col := d.C("counters")
 	change := mgo.Change{Update: bson.M{"$inc": bson.M{"n": 1}}, New: true, Upsert: true}
 	err := col.Find(bson.M{"_id": "C" + id}).Modify(change, &counter)
 	if err != nil {
@@ -43,9 +45,9 @@ func updateCounter(id string, s *mgo.Session) int {
 	return counter.N
 }
 
-func getCounter(id string, s *mgo.Session) int {
+func getCounter(id string, d *mgo.Database) int {
 	var counter Counter
-	col := s.DB("muteswan").C("counters")
+	col := d.C("counters")
 	err := col.Find(bson.M{"_id": "C" + id}).One(&counter)
 	if err != nil {
 		return(0)
@@ -54,21 +56,21 @@ func getCounter(id string, s *mgo.Session) int {
 	return counter.N
 }
 
-func PostMsg(c *goweb.Context, s *mgo.Session) {
+func PostMsg(c *goweb.Context, d *mgo.Database) {
 	var m Msg
 	var mw MsgWrap
 
-	col := s.DB("muteswan").C("C" + c.PathParams["hash"])
+	col := d.C("C" + c.PathParams["hash"])
 	body, _ := ioutil.ReadAll(c.Request.Body)
 	json.Unmarshal(body, &m)
 	mw.Content = m
-	mw.Id = updateCounter(c.PathParams["hash"], s)
+	mw.Id = updateCounter(c.PathParams["hash"], d)
 	mw.Timestamp = time.Now().Format(time.RFC1123)
 	mw.Timestamp = strings.Replace(mw.Timestamp,"UTC","GMT",-1)
 	col.Insert(mw)
 }
 
-func GetMsg(c *goweb.Context, s *mgo.Session) {
+func GetMsg(c *goweb.Context, d *mgo.Database) {
 
 	if strings.Contains(c.PathParams["id"], "-") {
 
@@ -81,7 +83,7 @@ func GetMsg(c *goweb.Context, s *mgo.Session) {
 		}
 
 		var msgs []MsgWrap
-		col := s.DB("muteswan").C("C" + c.PathParams["hash"])
+		col := d.C("C" + c.PathParams["hash"])
 		//msgQuery := col.Find(bson.M{"_id": bson.M{"$lte": top, "$gte": bottom}})
 		msgQuery := col.Find(bson.M{"_id": bson.M{"$gte": bottom, "$lte": top}}).Sort("-_id")
 		msgQuery.All(&msgs)
@@ -92,7 +94,7 @@ func GetMsg(c *goweb.Context, s *mgo.Session) {
 
 	} else {
 		var mw MsgWrap
-		col := s.DB("muteswan").C("C" + c.PathParams["hash"])
+		col := d.C("C" + c.PathParams["hash"])
 		id, _ := strconv.Atoi(c.PathParams["id"])
 		err := col.Find(bson.M{"_id": id}).One(&mw)
 		if err != nil {
@@ -107,17 +109,39 @@ func GetMsg(c *goweb.Context, s *mgo.Session) {
 
 }
 
-func GetLastMsg(c *goweb.Context, s *mgo.Session) {
+func GetLastMsg(c *goweb.Context, d *mgo.Database) {
 
-	mostRecent := getCounter(c.PathParams["hash"], s)
+	mostRecent := getCounter(c.PathParams["hash"], d)
 
 	fmt.Fprintf(c.ResponseWriter, `{"lastMessage":"%d"}`, mostRecent)
+}
+
+func dropPrivs() {
+	uid := 1001
+
+	//fmt.Printf("Current uid: %d\n", syscall.Getuid())
+	syscall.Setuid(uid)
+	//fmt.Printf("New uid: %d\n", syscall.Getuid())
+
 }
 
 func main() {
 
 	fmt.Printf("Max procs: %d\n", runtime.GOMAXPROCS(8))
 	fmt.Printf("Max procs: %d\n", runtime.GOMAXPROCS(8))
+
+	var port int
+	var ip string
+	var db string
+	flag.IntVar(&port, "port", 80, "Port to bind on.")
+	flag.StringVar(&ip, "ip", "127.0.0.1", "IP to bind to")
+	flag.StringVar(&db, "db", "muteswan", "MongoDB database to use")
+	flag.Parse()
+
+	fmt.Printf("Port is %d\n", port)
+	fmt.Printf("IP is %s\n", ip)
+	fmt.Printf("DB is %s\n", db)
+
 
 	session, err := mgo.Dial("localhost")
 	if err != nil {
@@ -128,26 +152,29 @@ func main() {
 	session.SetMode(mgo.Monotonic, true)
 
 	goweb.MapFunc("/{hash}/{id}", func(c *goweb.Context) {
-		//fmt.Printf("GET %s\n", c.Request.URL.String())
+		dropPrivs()
+		fmt.Printf("GET %s\n", c.Request.URL.String())
 		s := session.Copy()
-		GetMsg(c, s)
+		GetMsg(c, s.DB(db))
 		s.Close()
 	})
 
 	goweb.MapFunc("/{hash}", func(c *goweb.Context) {
-		//fmt.Printf("GET %s\n", c.Request.URL.String())
+		dropPrivs()
+		fmt.Printf("GET %s\n", c.Request.URL.String())
 		s := session.Copy()
-		GetLastMsg(c, s)
+		GetLastMsg(c, s.DB(db))
 		s.Close()
 	}, goweb.GetMethod)
 
 	goweb.MapFunc("/{hash}", func(c *goweb.Context) {
-		//fmt.Printf("POST %s\n", c.Request.URL.String())
+		dropPrivs()
+		fmt.Printf("POST %s\n", c.Request.URL.String())
 		s := session.Copy()
-		PostMsg(c, s)
+		PostMsg(c, s.DB(db))
 		s.Close()
 	}, goweb.PostMethod)
 
-	goweb.ListenAndServe(":8081")
+	goweb.ListenAndServe(fmt.Sprintf("%s:%d",ip,port))
 
 }
